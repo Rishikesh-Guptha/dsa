ff5403b656407e5c4c653867d6bf676ce64e2390
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-21 22:40:16 +0530

    added the first basics file

---
+++ basics1.java
@@ -0,0 +1,13 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class basics1 {
+    public static void main(String[] args){
+        int a=10;
+        Scanner scanner =new Scanner(System.in);
+        int b= scanner.nextInt();
+
+        System.out.println("hello world" + b);
+    }
+}
