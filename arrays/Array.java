6d48d7627bead8a443fb47f5a9df2049b38ac751
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-22 22:57:01 +0530

    Created the Arrays section

---
+++ arrays/Array.java
@@ -0,0 +1,21 @@
+package dsa;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void  main(String[] args){
+
+        //Arrays are Linear static ds where datas are stored in continous memory locations
+
+        //Initialization or Creation
+
+        int[] array1 ={1,2,3,4,5};
+        int[] array2= new int[5];
+
+        //Printing
+
+        System.out.println(array1);
+        System.out.println(array1[0]);
+        System.out.println(Arrays.toString(array1));
+    }
+}
