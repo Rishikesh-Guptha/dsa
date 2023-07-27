46cb4e9779bb307b6a045249227618a76e05f9fe
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-27 22:28:52 +0530

    added some content in scope

--- scopes.java
+++ scopes.java
@@ -7,11 +7,20 @@ public class scopes {
         System.out.println("a:"+a);
         {
             //Block scope
-            a=20;
-            int c=100;
+            a=20; // a is already initialized outside , so modifiying it inside a block does change it
+            int c=100; // c is initialized only inside the block , so not available outside the block
             System.out.println("c:"+c);
         }
         System.out.println("a is:"+a);
         System.out.println("c is:"+"This statement give me error");
+        change_value(a);
+        System.out.println("a is after function:"+a); // The value of a is not changing even after changed in the function
+
     }
+    static void change_value(int a){
+        a=100;
+        System.out.println("number:"+a);
+    };
+
+
 }
