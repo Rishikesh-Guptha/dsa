fdf15a9a7a3666f4f2d9b0658cb8ba33eb942780
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-27 22:20:19 +0530

    added some content in scope

--- scopes.java
+++ scopes.java
@@ -5,5 +5,13 @@ public class scopes {
         int a=10;
         int b=20;
         System.out.println("a:"+a);
+        {
+            //Block scope
+            a=20;
+            int c=100;
+            System.out.println("c:"+c);
+        }
+        System.out.println("a is:"+a);
+        System.out.println("c is:"+"This statement give me error");
     }
 }
