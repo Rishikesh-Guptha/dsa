cb1238f598ef83a5d816fdc4e2def899abe4841d
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-09-22 19:47:07 +0530

    Added content about Inheritance and classes in basic3

--- basic2.java
+++ basic2.java
@@ -1,35 +1,30 @@
 package dsa;
+
+import java.util.Arrays;
+
 // Constructors notes
 public class basic2 {
-    int length;
-    int breadth;
-    basic2() {
-        length=0;
-        breadth=0;
-    }
-    basic2(int a){
-        length=breadth=a;
-    }
-    basic2(int a, int b){
-        length=a;
-        breadth=b;
-    }
-    int area(){
-        return length*breadth;
+    public static void main(String[] args) {
+        String str[]={"ball","dog","cat","apple"};
+        String extra=null;
+        for (int i = 0; i <str.length ; i++) {
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].compareTo(str[i])>0){
+                    extra=str[i];
+                    str[i]=str[j];
+                    str[j]=extra;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(str));
+        System.out.println(str[0].compareTo(str[3]));
+        String a="hello";
+        int z=10;
+        System.out.println(a.indexOf('l',4));
+
     }
 }
 
- class one{
-     public static void main(String[] args) {
-         basic2 obj=new basic2();
-         basic2 obj1=new basic2(5);
-         basic2 obj2=new basic2(5,10);
-         int a =obj.area();
-         int b =obj1.area();
-         int c =obj2.area();
-         System.out.println(a);
-         System.out.println(b);
-         System.out.println(c);
-     }
 
-}
+
